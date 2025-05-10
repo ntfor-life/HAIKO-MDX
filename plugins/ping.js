5516839b02ee86a40b87f9c1975d52f9cf348e73
Professeur-xd <xdprofesseur@gmail.com>
2025-05-10 10:23:30 -0400

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -8,7 +8,7 @@ const ping = async (m, Matrix) => {
     const start = new Date().getTime();
 
     const reactionEmojis = ['🎗️', '🍁', '🚀', '⛓️', '🪄', '🐳', '🍓', '🌻', '🩸', '🧩'];
-    const textEmojis = ['🎗️', '🪅', '🪃', '💡', '📑', '⌛', '🔔', '🔮', '📜', '🛎️'];
+    const textEmojis = ['♻️', '🪅', '🪃', '💡', '📑', '⌛', '🔔', '🔮', '📜', '🛎️'];
 
     const reactionEmoji = reactionEmojis[Math.floor(Math.random() * reactionEmojis.length)];
     let textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
@@ -23,7 +23,7 @@ const ping = async (m, Matrix) => {
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
 
-    const text = `*HAIKO-MDX SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+    const text = `*HAIKO-MDX SPEED: ${responseTime.toFixed(2)}MS ${reactionEmoji}*`;
 
     await Matrix.sendMessage(m.from, {
       text,
